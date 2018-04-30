ea2a880fd577fb79fdad1b99045b545ff4329638
Mat Mariani <someone.mariani@gmail.com>
2018-04-29 22:36:57 -0400

    design overhaul
    may contain lorem ipsum!

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var cp = require('child_process');
+
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+gulp.task('jekyll-build', function (done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.reload();
+});
+
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+});
+
+gulp.task('sass', function () {
+	return gulp.src('_scss/main.scss')
+	  .pipe(sass({
+	    includePaths: ['scss'],
+	    onError: browserSync.notify
+	  }))
+	  .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+	  .pipe(gulp.dest('_site/css'))
+	  .pipe(browserSync.reload({stream:true}))
+	  .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('_scss/*.scss', ['sass']);
+  gulp.watch(['*.html', '*.css', '_data/*.yml', '_includes/*.html', '_layouts/*.html'], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
