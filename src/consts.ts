1f26bd0cb02e2c8de7a806f96069c99886b81a70
mathewmariani <someone.mariani@gmail.com>
2025-03-07 10:46:00 -0500

    Small text updates

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,7 @@ export const SITE_DESCRIPTION = '';
 export const HomeLink = (): string => '/';
 export const ResumeLink = (): string => '/resume';
 export const ProjectsLink = (): string => '/projects';
+export const CoursesLink = (): string => '/courses';
+
+export const UbisoftMontreal = (): string => 'https://www.linkedin.com/company/ubisoft-montreal/';
+export const StockholmSyndromeAI = (): string => 'https://www.linkedin.com/company/stosynai/';
