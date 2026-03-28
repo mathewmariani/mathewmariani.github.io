e8930a9170bb6b8e27ea0958889e716b03eb4e5a
mathewmariani <someone.mariani@gmail.com>
2026-03-28 17:48:11 -0400

    Added new pages

--- src/consts.ts
+++ src/consts.ts
@@ -5,6 +5,8 @@ export const HomeLink = (): string => '/';
 export const ResumeLink = (): string => '/resume';
 export const ProjectsLink = (): string => '/projects';
 export const CoursesLink = (): string => '/courses';
+export const SimulatorsLink = (): string => '/simulators';
+export const GamesLink = (): string => '/games';
 
 export const UbisoftMontreal = (): string => 'https://www.linkedin.com/company/ubisoft-montreal/';
 export const StockholmSyndromeAI = (): string => 'https://www.linkedin.com/company/stosynai/';
